c16decc6d2905e4eadb051342ced112c3b1a5324
obornandrew <obo09001@byui.edu>
2019-07-13 15:16:01 -0600

    update

--- assignments/lesson-9/preston-9/scripts/windchill.js
+++ assignments/lesson-9/preston-9/scripts/windchill.js
@@ -1,5 +1,5 @@
-var temp = 13;
-var windSpeed = 18;
+var temp = 75;
+var windSpeed = 10;
 var windChill = 35.74 + 0.6215 * temp - 35.75 * Math.pow(windSpeed, 0.16) + 0.4275 * temp * Math.pow(windSpeed, 0.16);
 
 document.getElementById('windChill').innerHTML = windChill.toFixed(1);
